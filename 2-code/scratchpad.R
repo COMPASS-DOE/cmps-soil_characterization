c8290f23e50cc42d42b013ea2ed013fa221cc059
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2024-03-15 10:07:17 -0700

    scratchpad

--- 2-code/scratchpad.R
+++ 2-code/scratchpad.R
@@ -762,3 +762,480 @@ icp_ic_long =
   drop_na() %>% 
   left_join(sample_key)
 icp_ic_long %>% write.csv("1-data/processed/synoptic_ICP_IC_all_horizons.csv", row.names = F)
+
+
+
+# -------------------------------------------------------------------------
+
+theme_set(theme_bw(base_size = 14))
+
+
+x = pca_overall$pca_int$rotation
+
+x2 = 
+  x %>% 
+  as.data.frame() %>% 
+  dplyr::select(PC1) %>% 
+  arrange(desc(PC1)) %>% 
+  rownames_to_column("variable") %>% 
+  mutate(variable = fct_reorder(variable, (PC1)))
+
+x2 %>% 
+  ggplot(aes(x = PC1, y = variable))+
+  geom_point(size = 3)
+
+
+
+pca_overall = fit_pca_function(data_combined_wide_NO_DIC) 
+
+data_wide = 
+  data_combined_wide_NO_DIC %>% 
+  dplyr::select(-c(gwc, Mg, Na))
+
+
+
+
+
+
+
+get_pc_values = function(dat){
+  
+  pca = fit_pca_function(dat)
+  
+  x = pca$pca_int$rotation
+  x2 = 
+    x %>% 
+    as.data.frame() %>% 
+    dplyr::select(PC1) %>% 
+    arrange(desc(PC1)) %>% 
+    rownames_to_column("variable") %>% 
+    mutate(variable = fct_reorder(variable, (PC1)))
+  
+  x2
+}
+
+# round 1
+pc_overall = get_pc_values(pca_overall) %>% mutate(region = "combined")
+pc_wle = get_pc_values(pca_overall_wle) %>% mutate(region = "WLE")
+pc_cb = get_pc_values(pca_overall_cb) %>% mutate(region = "CB")
+
+pc_all = 
+  bind_rows(pc_overall, pc_wle, pc_cb) %>% 
+  mutate(variable = fct_reorder(variable, (PC1)))
+
+pc_all %>% 
+  filter(region == "combined") %>% 
+  mutate(variable = fct_reorder(variable, (PC1))) %>% 
+  ggplot(aes(x = PC1, y = variable, color = region))+
+  geom_point(size = 3)+
+  xlim(-0.4, 0.4)
+
+
+# round 2
+pc_overall = get_pc_values(data_wide) %>% mutate(region = "combined")
+pc_wle = get_pc_values(data_wide %>% filter(region == "WLE")) %>% mutate(region = "WLE")
+pc_cb = get_pc_values(data_wide %>% filter(region == "CB")) %>% mutate(region = "CB")
+
+pc_all = 
+  bind_rows(pc_overall, pc_wle, pc_cb) %>% 
+  mutate(variable = fct_reorder(variable, (PC1)))
+
+pc_all %>% 
+  mutate(variable = fct_reorder(variable, (PC1))) %>% 
+  filter(region != "combined") %>% 
+  ggplot(aes(x = PC1, y = variable, color = region))+
+  geom_point(size = 5)+
+  scale_colour_manual(values = c("#81C0A7", "#1A421D"))+
+  xlim(-0.4, 0.4)
+
+
+# uplands 
+pc_upland = get_pc_values(data_wide %>% filter(transect == "upland")) %>% 
+  mutate(transect = "upland")
+
+pc_upland %>% 
+  mutate(variable = fct_reorder(variable, (PC1))) %>% 
+  #filter(region != "combined") %>% 
+  ggplot(aes(x = (PC1), y = variable, 
+             #color = region
+  ))+
+  geom_point(size = 3)+
+  xlim(-0.4, 0.4)
+
+
+# wetland 
+pc_wetland = get_pc_values(data_wide %>% filter(transect == "wc")) %>% 
+  mutate(transect = "wetland")
+
+pc_transition = get_pc_values(data_wide %>% filter(transect == "transition")) %>% 
+  mutate(transect = "transition")
+
+
+pc_wetland %>% 
+  mutate(variable = fct_reorder(variable, (PC1))) %>% 
+  #filter(region != "combined") %>% 
+  ggplot(aes(x = (PC1), y = variable, 
+             #color = region
+  ))+
+  geom_point(size = 3)+
+  xlim(-0.4, 0.4)
+
+pc_upland_wetland = 
+  bind_rows(pc_upland, pc_wetland, pc_transition)
+
+pc_upland_wetland %>% 
+  mutate(variable = fct_reorder(variable, abs(PC1))) %>% 
+  #filter(region != "combined") %>% 
+  ggplot(aes(x = (PC1), y = variable, 
+             color = transect
+  ))+
+  geom_point(size = 5)+
+  xlim(-0.4, 0.4)
+
+
+
+data_combined_wide = make_data_wide(data_combined, sample_key)
+
+
+#
+# 
+
+# Z score ----
+
+z_score_df <- 
+  data_combined %>% 
+  filter(transect != "wte") %>% 
+  group_by(analysis, name, region, site, transect, horizon) %>% 
+  dplyr::mutate(median = median(value),
+                mean = mean(value),
+                sd = sd(value),
+                z_score = (value - mean) / sd)
+
+
+z_score_df %>% 
+  ggplot(aes(x = abs(z_score), y = name, color = site)) +
+  geom_point()+
+  facet_wrap(~ region + transect)
+
+z_score_df %>% 
+  ggplot(aes(x = abs(z_score), y = name, color = site)) +
+  geom_point()+
+  facet_wrap(~ region + transect)
+
+
+z_score_df %>% 
+  ggplot(aes(x = abs(z_score), y = name, color = site)) +
+  geom_segment(aes(x = min(z_score), xend = max(z_score), y = name, yend = name))+
+  facet_wrap(~ region + transect)
+
+
+
+
+
+
+z_score_df %>% 
+  group_by(region, transect, horizon, name) %>% 
+  dplyr::summarise(z_min = min(z_score),
+                   z_max = max(z_score)) %>% 
+  ggplot() +
+  geom_segment(aes(x = z_min, xend = z_max, y = name, yend = name),
+               linewidth = 1)+
+  geom_point(data = z_score_df, aes(x = z_score, y = name))+
+  facet_wrap(~ region + transect)
+  
+  
+  
+z_score_df %>% 
+ggplot(aes(x = transect, y = z_score, color = name, fill = name))+
+  ggdist::stat_halfeye(
+                       size = 1, alpha = 0.5,
+                       position = position_nudge(x = 0.2), width = 0.5, 
+                       #slab_color = "black"
+  )+
+  geom_jitter(width = 0.1, aes(color = name))+
+  facet_wrap(~region)+
+  theme_bw()
+
+
+z_score_df %>% 
+  ggplot(aes(y = name, x = z_score, color = transect, fill = transect))+
+  ggdist::stat_halfeye(
+    size = 1, alpha = 0.5,
+    #position = position_nudge(x = 0.2), width = 0.5, 
+    #slab_color = "black"
+  )+
+  geom_jitter(height = 0.1)+
+  facet_wrap(~region)+
+  theme_bw()
+
+  
+  
+  
+  
+
+library(gtrendsR)
+library(tidyverse)
+library(ggridges)
+library(showtext)
+library(ggalt)
+
+z_score_df %>% 
+  filter(region == "CB") %>% 
+  ggplot(aes(x = z_score, color = transect, fill = transect, group = transect))+
+  geom_density(alpha = 0.2)+
+  facet_grid(name ~ region)
+
+
+
+# normalized distances ----------------------------------------------------
+
+data_normalized <- 
+  data_combined_wide %>% 
+  dplyr::select(where(is.numeric)) %>% 
+  scale() %>% 
+  bind_cols(data_combined_wide %>% dplyr::select(where(is.character))) %>%
+  filter(transect != "wte") %>% 
+  pivot_longer(cols = where(is.numeric)) 
+  
+data_normalized_summary <- 
+  data_normalized %>% 
+  group_by(region, site, transect, horizon, name) %>% 
+  dplyr::summarise(value = median(value, na.rm = T))
+  
+
+data_normalized_summary %>% 
+  ggplot(aes(x = value, y = name, color = transect))+
+  geom_point(size = 3)+
+  #facet_wrap(~region)
+  facet_wrap(~region+site)
+
+  
+data_normalized %>% 
+  ggplot(aes(x = value, y = name, color = transect))+
+ # geom_violin()+
+  geom_point()+
+  facet_wrap( ~ region)
+  
+data_normalized_summary2 <- 
+  data_normalized_summary %>% 
+  pivot_wider(names_from = "transect") %>% 
+  mutate(distance_transition = transition - transition,
+         distance_upland = upland - transition,
+         distance_wetland = wc - transition) %>% 
+  dplyr::select(region, site, horizon, name, starts_with("distance")) %>% 
+  pivot_longer(cols = c(distance_transition, distance_upland, distance_wetland),
+               names_to = "transect")
+
+
+data_normalized_summary2 %>% 
+  ggplot(aes(x = value, y = name, color = transect))+
+  geom_segment(aes(x = 0, xend = value, y = name, yend = name))+
+  geom_point(size = 3)+
+  labs(x = "normalized distance from transition")+
+  #facet_wrap(~region)
+  facet_wrap(~region + site)
+
+
+
+
+
+
+data_combined_subset %>% 
+  filter(name == "mehlichp_ugg") %>% 
+  ggplot(aes(x = transect, y = value, color = site))+
+  geom_jitter(width = 0.1)+
+  facet_wrap(~region)
+
+data_combined_subset %>% 
+  filter(name == "Fe_ugg") %>% 
+  ggplot(aes(x = transect, y = value, color = site))+
+  geom_jitter(width = 0.1)+
+  facet_wrap(~region)
+
+
+# euclidean distance ----
+library(vegan)
+library(ape)
+
+compute_euclidean_distance <- function(data_normalized){
+  
+  wide = 
+    data_normalized %>% 
+    #dplyr::select(-sample_label, -tree_number, -horizon) %>% 
+    pivot_wider(names_from = "transect", values_from = "value") 
+  
+  upland = wide %>% pull(upland) %>% na.omit()
+  transition = wide %>% pull(transition) %>% na.omit()
+  wetland = wide %>% pull(wc) %>% na.omit()
+  
+  
+  
+  tibble(
+      euclidean_dist_up_tr = dist(rbind(upland, transition)),
+      euclidean_dist_wc_tr = dist(rbind(wetland, transition)),
+      median_tr = mean(transition),
+      median_up = mean(upland),
+      median_wc = mean(wetland)
+    ) %>% 
+    mutate(euclidean_dist_up_tr = as.numeric(euclidean_dist_up_tr),
+           euclidean_dist_wc_tr = as.numeric(euclidean_dist_wc_tr),
+           sign_up = if_else(median_up - median_tr <= 0, -1, 1),
+           sign_wc = if_else(median_wc - median_tr <= 0, -1, 1),
+           euclidean_dist_up_tr = sign_up * euclidean_dist_up_tr,
+           euclidean_dist_wc_tr = sign_wc * euclidean_dist_wc_tr,
+           euclidean_dist_tr = 0)
+  
+  
+  
+  
+  }
+
+euc <- 
+  data_normalized %>% 
+  filter(name != "percentOM") %>% 
+  group_by(region, site, name) %>% 
+  do(compute_euclidean_distance(.))
+
+
+
+
+
+euc %>% 
+  pivot_longer(cols = c(euclidean_dist_up_tr, euclidean_dist_wc_tr, euclidean_dist_tr), names_to = "transect", values_to = "distance") %>% 
+  ggplot(aes(x = distance, y = name, color = transect))+
+  geom_segment(aes(xend = 0, yend = name))+
+  geom_point(size = 3)+
+  labs(x = "Euclidean distance")+
+  facet_wrap(~region + site)+
+  theme_kp()
+
+
+x <- array(1 : 9 , dim = c(3, 3, 2))
+
+y <- array(10: 19, dim = c(3, 3, 2))
+
+
+
+
+
+
+
+
+# make matrices ----
+# 
+num = data_wide_PCA %>% drop_na() %>% dplyr::select(where(is.numeric))
+grp = data_wide_PCA %>% drop_na() %>% dplyr::select(where(is.character)) %>% rownames_to_column("row") %>% mutate(row = as.numeric(row))
+
+bray_distance = vegdist(num, method="euclidean")
+principal_coordinates = pcoa(bray_distance)
+pcoa_plot = data.frame(principal_coordinates$vectors[,])
+pcoa_plot_merged = merge(pcoa_plot, grp, by="row.names")
+
+grp = 
+  grp %>% 
+  dplyr::mutate(grp = paste0(region,"-",site,"-",transect))
+#dplyr::select(row, grp)
+matrix = as.matrix(bray_distance)
+
+matrix2 = 
+  matrix %>% 
+  data.table::melt() %>% 
+  left_join(grp, by = c("Var1"="row")) %>% 
+  #rename(grp1 = grp) %>% 
+  left_join(grp, by = c("Var2"="row")) %>% 
+  filter(grp.x != grp.y) %>% 
+  filter(site.x == site.y) %>% 
+  group_by(region.x, site.x, transect.x, transect.y) %>% 
+  dplyr::summarise(distance = mean(value)) %>%
+  ungroup
+  rename(sat_level = sat_level.x) %>% 
+  dplyr::mutate(sat_level = if_else(treatment.x=="FM","FM",sat_level),
+                sat_level = factor(sat_level, levels = c(5,35,50,75,100,"FM")))
+
+matrix3 = 
+  matrix %>% 
+  melt() %>% 
+  left_join(grp, by = c("Var1"="row")) %>% 
+  #rename(grp1 = grp) %>% 
+  left_join(grp, by = c("Var2"="row")) %>% 
+  filter(!grp.x==grp.y) %>% 
+  filter(sat_level.x == sat_level.y) %>% 
+  filter(texture.x == texture.y) %>% 
+  group_by(grp.x,grp.y,sat_level.x, texture.x,treatment.x,treatment.y) %>% 
+  dplyr::summarise(distance  =mean(value)) %>%
+  ungroup %>% 
+  rename(sat_level = sat_level.x) %>% 
+  dplyr::mutate(sat_level = factor(sat_level, levels = c(5,35,50,75,100)))
+
+
+eucl_label = tribble(
+  ~x, ~xend, ~y, ~yend, ~texture.x, ~type,
+  0.7, NA , 3, NA, "SCL", "text",
+  0.7, 0.7, 2, 2.8, "SCL", "curve"
+)
+
+# make plot ----
+fticr_eucl = 
+  ggplot(matrix3, aes(x = sat_level, y = distance))+
+  geom_point(size=3)+
+  geom_segment(aes(x = sat_level, xend = sat_level, y = 0, yend = distance))+
+  
+  ylim(0,10)+
+  labs(x = "% saturation",
+       y = "drying-rewetting \n Euclidean distance")+
+  geom_hline(yintercept = 1.713, linetype = "dashed")+
+  
+  theme_kp()
+```
+
+
+
+# pairwise permanova ----
+
+library("vegan")
+pairwise_permanova <- function(sp_matrix, group_var, dist = "bray", adj = "fdr", perm = 10000) {
+  
+  require(vegan)
+  
+  ## list contrasts
+  group_var <- as.character(group_var)
+  groups <- as.data.frame(t(combn(unique(group_var), m = 2)))
+  
+  contrasts <- data.frame(
+    group1 = groups$V1, group2 = groups$V2,
+    R2 = NA, F_value = NA, df1 = NA, df2 = NA, p_value = NA
+  )
+  
+  for (i in seq(nrow(contrasts))) {
+    sp_subset <- group_var == contrasts$group1[i] | group_var == contrasts$group2[i] 
+    contrast_matrix <- sp_matrix[sp_subset,]
+    
+    ## fit contrast using adonis
+    fit <- vegan::adonis2(
+      contrast_matrix ~ group_var[sp_subset],
+      method = dist, 
+      perm = perm
+    )
+    
+    contrasts$R2[i] <- round(fit$R2[1], digits = 3)
+    contrasts$F_value[i] <- round(fit[["F"]][1], digits = 3)
+    contrasts$df1[i] <- fit$Df[1]
+    contrasts$df2[i] <- fit$Df[2]
+    contrasts$p_value[i] <- fit$`Pr(>F)`[1]
+  }
+  
+  ## adjust p-values for multiple comparisons
+  contrasts$p_value <- round(p.adjust(contrasts$p_value, method = adj), digits = 3)
+  
+  return(list(
+    contrasts = contrasts, 
+    "p-value adjustment" = adj, 
+    permutations = perm
+  ))
+}
+
+pairwise_permanova(dune, groups)
+data("dune", package = "vegan")
+
+pairwise_permanova(sp_matrix = data_wide_PCA %>% filter(region == "CB") %>% drop_na() %>% dplyr::select(where(is.numeric)), 
+                   group_var = data_wide_PCA %>% filter(region == "CB") %>% drop_na() %>% dplyr::pull(transect))
