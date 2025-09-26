f2a37bb1c6ff2424d3b4f01e0d5467852e0431d6
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2025-09-26 12:09:25 -0700

    update targets. export physics data

--- _targets.R
+++ _targets.R
@@ -13,7 +13,7 @@ tar_option_set(
 
 # Load the R scripts with your custom functions:
 source("2-code/0-packages.R")
-#source("2-code/1-initial_processing.R")
+#source("2-code/1-initial_processing.R") # use this only once, to pull the necessary sample weights, etc.
 source("2-code/2-functions_processing.R")
 source("2-code/3a-functions_soil_moisture_sensors.R")
 
@@ -28,7 +28,7 @@ list(
   tar_target(analysis_key_data, "1-data/analysis_key.csv", format = "file"),
   tar_target(analysis_key, read.csv(analysis_key_data)),
   
-  # data files
+  # data files - chemistry
   tar_target(moisture_data, import_gsheet("1nYzExPmpv01IYalqo2L3pmDsBDvhjdvHYMJo31N5_MA")),
   tar_target(moisture_processed, process_moisture(moisture_data)),
   tar_target(loi_data, import_loi(FILEPATH = "1-data/raw/loi")),
@@ -63,30 +63,19 @@ list(
   tar_target(wrc_processed, process_wrc(wrc_data)),
   tar_target(wrc_parameters, import_wrc_parameters(FILEPATH = "1-data/raw/wrc")),
   tar_target(texture_processed, compute_texture(FILEPATH = "https://docs.google.com/spreadsheets/d/13yOYC7vVzVzatXgJUaey2RYdmsJnfnmLmnVWVt4mOXo/", sample_key)),
-  # bulk density
+  tar_target(bd_data_file, "1-data/raw/bulk_density.csv", format = "file"),
+  tar_target(bd_data, read.csv(bd_data_file)),
   tar_target(pd_data, read_sheet("https://docs.google.com/spreadsheets/d/1wHmiOvxa2SQhiGLIiUkNqiDpJQk__XjfrmA1fyuVYEA/")),
   tar_target(pd_processed, process_pd(pd_data)),
-
+  
   # combined data
   tar_target(data_combined_all_horizons, combine_data(moisture_processed, pH_processed, tctnts_data_samples, loi_processed,
-                                         weoc_processed, dic_processed, din_processed, icp_processed,
-                                         ferrozine_processed, mehlich_processed, ions_processed_meq, 
-                                         sample_key)),
+                                                      weoc_processed, dic_processed, din_processed, icp_processed,
+                                                      ferrozine_processed, mehlich_processed, ions_processed_meq, 
+                                                      sample_key)),
   tar_target(data_combined, subset_surface_horizons(data_combined_all_horizons)),
   tar_target(data_combined_wide, make_data_wide_processing(data_combined, sample_key)),
   tar_target(summary_tables, make_summary_tables(data_combined, pd_processed, xrd_processed, texture_processed, wrc_parameters)),
-  
-  # export
-  tar_target(export, {
-    write.csv(data_combined, "1-data/processed/chemistry_combined_surface_horizon.csv", row.names = FALSE)
-    write.csv(data_combined_all_horizons, "1-data/processed/chemistry_combined_all_horizons.csv", row.names = FALSE)
-    write.csv(data_combined_wide, "1-data/processed/chemistry_combined_wide.csv", row.names = FALSE)
-    # texture
-    # PD
-    # BD
-    # WRC
-  }, format = "file"),
-  
   
   # soil moisture sensors
   tar_target(sensor_df_trim, process_teros(SENSOR_PATH = "1-data/soil_moisture_sensors/csvs_to_process")),
@@ -95,9 +84,24 @@ list(
   tar_target(troll, calculate_water_levels(sensor_df_trim)),
   tar_target(percent_flooded, calculate_percent_flooded(troll)),
   tar_target(water_table, summarize_water_table(troll)),
-
-   # report  
-#   tar_render(report, path = "3-reports/characterization_report.Rmd")
-   tar_render(report, path = "3-reports/manuscript_figures.Rmd")
-
+  
+  # export
+  tar_target(export, {
+    write.csv(data_combined, "1-data/processed/chemistry_combined_surface_horizon.csv", row.names = FALSE)
+    write.csv(data_combined_all_horizons, "1-data/processed/chemistry_combined_all_horizons.csv", row.names = FALSE)
+    write.csv(data_combined_wide, "1-data/processed/chemistry_combined_wide.csv", row.names = FALSE)
+    
+    write.csv(bd_data, "1-data/processed/physics_mineralogy/bulk_density.csv", row.names = FALSE)
+    write.csv(pd_processed, "1-data/processed/physics_mineralogy/particle_density.csv", row.names = FALSE)
+    write.csv(texture_processed, "1-data/processed/physics_mineralogy/texture.csv", row.names = FALSE)
+    write.csv(wrc_processed, "1-data/processed/physics_mineralogy/water_retention_curve_data.csv", row.names = FALSE)
+    write.csv(wrc_parameters, "1-data/processed/physics_mineralogy/water_retention_curve_parameters.csv", row.names = FALSE)
+    write.csv(xrd_processed, "1-data/processed/physics_mineralogy/xrd_mineralogy.csv", row.names = FALSE)
+    
+  }, format = "file"),
+  
+  # report  
+  #   tar_render(report, path = "3-reports/characterization_report.Rmd")
+  tar_render(report, path = "3-reports/manuscript_figures.Rmd")
+  
 )
